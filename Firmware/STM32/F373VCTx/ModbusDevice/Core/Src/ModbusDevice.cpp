0371f8b061765fc3a700c6e3c271ac804ab71989
Martin Dubois <mdubois@kms-quebec.com>
2023-05-30 14:56:56 -0400

    ModbusDevice Initial

---
+++ Firmware/STM32/F373VCTx/ModbusDevice/Core/Src/ModbusDevice.cpp
@@ -0,0 +1,16 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2023 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Samples
+// File      Firmware/STM32/F373VCTx/ModbusDevice/Core/Src/ModbusDevice.cpp
+
+// ===== Local ==============================================================
+#include "ModbusDevice.h"
+
+// Entry point
+// //////////////////////////////////////////////////////////////////////////
+
+void ModbusDevice_Main()
+{
+}
