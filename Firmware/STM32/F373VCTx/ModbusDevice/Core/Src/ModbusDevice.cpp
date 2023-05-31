c9d6dd82e9a98cd405cae14226036302c2ba6970
Martin Dubois <mdubois@kms-quebec.com>
2023-05-31 13:52:13 -0400

    Modbus device use KMS-Framwork

--- Firmware/STM32/F373VCTx/ModbusDevice/Core/Src/ModbusDevice.cpp
+++ Firmware/STM32/F373VCTx/ModbusDevice/Core/Src/ModbusDevice.cpp
@@ -5,8 +5,20 @@
 // Product   KMS-Samples
 // File      Firmware/STM32/F373VCTx/ModbusDevice/Core/Src/ModbusDevice.cpp
 
+#include <KMS/Base.h>
+
+// ===== Import/Includes ====================================================
+#include <KMS/STM/STM32F.h>
+
 // ===== Local ==============================================================
 #include "ModbusDevice.h"
+
+using namespace KMS;
+
+// Static variables
+// //////////////////////////////////////////////////////////////////////////
+
+static STM::STM32F sProcessor;
 
 // Entry point
 // //////////////////////////////////////////////////////////////////////////
