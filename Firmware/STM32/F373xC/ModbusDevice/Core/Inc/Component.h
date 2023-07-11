c046e88cd210b75d27a88da84c544ecb6582bad5
Martin Dubois <mdubois@kms-quebec.com>
2023-07-11 18:57:02 -0400

    Use KMS-Framework 0.1.75-stm32f

---
+++ Firmware/STM32/F373xC/ModbusDevice/Core/Inc/Component.h
@@ -0,0 +1,13 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2023 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Samples
+// File      Firmware/STM32/F373xC/ModbusDevice/Core/Inc/Component.h
+
+#pragma once
+
+#include <KMS/Base.h>
+
+// ===== C ==================================================================
+#include <stdint.h>
